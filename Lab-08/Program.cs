d6a09a704ded4f0b0d1b6806101656e33c422a9e
FernandoValdivia <luis.valdivia.m@tecsup.edu.pe>
2021-10-08 21:56:21 -0500

    Lambda Expression added

--- Lab-08/Program.cs
+++ Lab-08/Program.cs
@@ -17,10 +17,18 @@ namespace Lab_08
             //Ordering();
             //Grouping();
             //Grouping2();
-            Joinning();
+            //Joinning();
+
+            //IntroToLambda();
+            //DataSourceLambda();
+            //FilteringLambda();
+            //OrderingLambda();
+            //GroupingLambda();
+            //Grouping2Lambda();
+            JoinningLambda();
             Console.Read();
         }
-        
+        //LINQ
         static void IntroToLINQ()
         {
             int[] numbers = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
@@ -104,6 +112,83 @@ namespace Lab_08
                             from cust in context.clientes
                             join dist in context.Pedidos on cust.idCliente equals dist.IdCliente
                             select new { CustomerName = cust.NombreCompañia, DistributorName = dist.PaisDestinatario};
+
+            foreach (var item in innerJoinQuery)
+            {
+                Console.WriteLine(item.CustomerName);
+            }
+        }
+        //Lambda
+        static void IntroToLambda()
+        {
+            int[] numbers = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
+
+            var numQuery =
+                numbers.Where(x => (x % 2 == 0)).Select(x => x).ToList();
+            foreach (int num in numQuery)
+            {
+                Console.Write("{0,1} ", num);
+            }
+        }
+        static void DataSourceLambda()
+        {
+            var queryAllCustomers = context.clientes.Select(c => c);
+
+            foreach (var item in queryAllCustomers)
+            {
+                Console.WriteLine(item.NombreCompañia);
+            }
+        }
+        static void FilteringLambda()
+        {
+            var queryLondonCustomers = context.clientes.Where(c => c.Ciudad == "Londres").Select(c => c);
+
+            foreach (var item in queryLondonCustomers)
+            {
+                Console.WriteLine(item.Ciudad);
+            }
+        }
+        static void OrderingLambda()
+        {
+            var queryLondonCustomers = context.clientes.Where(c => c.Ciudad == "Londres").OrderBy(c => c.NombreCompañia).Select(c => c);
+
+            foreach (var item in queryLondonCustomers)
+            {
+                Console.WriteLine(item.Ciudad);
+            }
+        }
+        static void GroupingLambda()
+        {
+            IQueryable<IGrouping<string, clientes>> queryCustomersByCity = context.clientes.GroupBy(c => c.Ciudad).Select(c => c);
+
+            foreach (var customerGroup in queryCustomersByCity)
+            {
+                Console.WriteLine(customerGroup.Key);
+                foreach (clientes customer in customerGroup)
+                {
+                    Console.WriteLine($"{customer.NombreCompañia}");
+                }
+            }
+        }
+        static void Grouping2Lambda()
+        {
+            var custQuery = context.clientes.GroupBy(c => c.Ciudad).Where(c => c.Count() > 2).OrderBy(c => c.Key).Select(c => c);
+            foreach (var item in custQuery)
+            {
+                Console.WriteLine(item.Key);
+                foreach (clientes customer in item)
+                {
+                    Console.WriteLine($"{customer.NombreCompañia}");
+                }
+            }
+        }
+        static void JoinningLambda()
+        {
+            var innerJoinQuery = context.clientes.Join(context.Pedidos, c => c.idCliente, 
+                                                        p => p.IdCliente, (c, p) => new { 
+                                                            CustomerName = c.NombreCompañia, 
+                                                            DistributorName = p.PaisDestinatario 
+                                                            });
 
             foreach (var item in innerJoinQuery)
             {
